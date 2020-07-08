c5adc0af1ba605b210a7ed3aca518d46e50c5c05
Timothy Graupmann <tim.graupmann@razer.com>
2020-07-08 14:34:06 -0700

    Initial commit of project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// This is a project to add support for C++ features:
+//
+// libCurl - HTTPS POST
+// jsoncpp - Constructing JSON
+// libzip - Compressing ASCII Text
+//
+
+#include <stdio.h>
+
+int main()
+{
+	fprintf(stdout, "Hello World!\r\n");
+	return 0;
+}
